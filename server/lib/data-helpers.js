fd22bc3803b433186e1d9ed3bc0815f26d77b7f1
Max Graham <m.huw.graham@gmail.com>
2019-01-24 19:24:17 +0000

    Uses mongo to getTweets

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -17,11 +17,22 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweeter").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
   };
 }
+
+
+  // function getTweets(callback)
+
+  //   {
+  //     simulateDelay(() => {
+  //       const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+  //       callback(null, db.tweets.sort(sortNewestFirst));
+  //     });
+  //   }
