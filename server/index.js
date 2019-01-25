22a45ac0462e9e8f71514601641172e977ff47fa
Max Graham <m.huw.graham@gmail.com>
2019-01-25 17:53:43 +0000

    SASS and node-sass-middleware installed.

--- server/index.js
+++ server/index.js
@@ -6,13 +6,24 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const sassMiddleware = require("node-sass-middleware");
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const MongoClient = require("mongodb").MongoClient;
-const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-
+app.use(
+  sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, "styles"),
+    dest: path.join(__dirname, "public", "styles"),
+    debug: true,
+    outputStyle: "expanded",
+    prefix: "/styles" // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+  })
+);
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
 
