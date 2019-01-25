3bed52ebd8dbb8dd88a03311371762f231106877
Max Graham <m.huw.graham@gmail.com>
2019-01-25 18:55:32 +0000

    SASS implemented, need to refactor SCSS files more.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 const sassMiddleware = require("node-sass-middleware");
+const path = require("path");
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
